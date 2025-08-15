fcba6f03e8e4895a787fa9924abe05631fa1a75d
Andres Morales <107047190+Blisse1@users.noreply.github.com>
2025-08-15 10:46:43 -0500

    interfaces finished

--- src/main/java/inheritance/Zoo.java
+++ src/main/java/inheritance/Zoo.java
@@ -3,13 +3,23 @@ package inheritance;
 public class Zoo {
     public static void main(String[] args) {
         Dog rocky = new Dog();
-        rocky.fetch();
-        rocky.makeSound();
+//        rocky.fetch();
+//        rocky.makeSound();
+        feed(rocky);
 
-        Animal sasha = new Dog();
-        sasha.makeSound();
+        Animal sasha = new Cat();
+//        sasha.makeSound();
+//        sasha = new Cat();
+//        sasha.makeSound();
+        feed(sasha);
+    }
 
-        sasha = new Cat();
-        sasha.makeSound();
+    // it can take Animal or any subclass of Animal
+    public static void feed(Animal animal){
+        if(animal instanceof Dog){
+            System.out.println("Here is your dog food");
+        } else if (animal instanceof Cat) {
+            System.out.println("Here is your cat food");
+        }
     }
 }
